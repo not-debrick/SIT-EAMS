968a3b37cbe3d4e9595c589185926fb9fdfb524b
Rick Jone De Leon Villanueva <110093390+not-debrick@users.noreply.github.com>
2025-03-11 20:52:20 +0800

    Updated data in db and added search function for students.

--- admin.php
+++ admin.php
@@ -2,10 +2,23 @@
     <?php
         include (__DIR__ . '/CONSTANTS/header.php');
         include (__DIR__ . '/CONSTANTS/navigation.php');
+        echo "<p class='display-4'> Students Attendance Search </p>";
         include 'searchExport.php';
 
         $query = "
             SELECT * FROM ". $tableMain . " INNER JOIN " . $tableStudentInfo . " ON " .$tableMain.".idNum = ". $tableStudentInfo .".id INNER JOIN " . $tableEventsList . " ON " .$tableMain.".eventsId = ". $tableEventsList .".id ORDER BY " .$tableMain.".id DESC ";
+        
+        /* include 'table_query.php'; */
+    ?>
+</div>
+
+<div class="container">
+    <?php
+        echo "<p class='display-4'> Students Search </p>";
+        include 'studentSearch.php';
+
+        /* $query = "
+            SELECT * FROM ". $tableMain . " INNER JOIN " . $tableStudentInfo . " ON " .$tableMain.".idNum = ". $tableStudentInfo .".id INNER JOIN " . $tableEventsList . " ON " .$tableMain.".eventsId = ". $tableEventsList .".id ORDER BY " .$tableMain.".id DESC "; */
         
         /* include 'table_query.php'; */
     ?>
